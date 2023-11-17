098778e2b7ddb2e3b99ab90b82b684fde7c2c758
gnostic <64601257+nynxa@users.noreply.github.com>
2023-11-17 00:31:14 -0500

    bugfix: rm clasp json

--- env.d.ts
+++ env.d.ts
@@ -2,9 +2,9 @@ declare module 'bun' {
     interface Env {
         CONVERT_KIT_API_KEY: string
         CONVERT_KIT_SECRET: string
-        APPS_SCRIPT_URL: string
         CONVERT_KIT_BASE_URL: string
         DEPLOYMENT_ID: string
         SCRIPT_ID: string
+        APPS_SCRIPT_URL: string
     }
 }
--- env2.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'bun' {
-    interface Env {
-        CONVERT_KIT_API_KEY: string
-        CONVERT_KIT_SECRET: string
-        CONVERT_KIT_BASE_URL: string
-        DEPLOYMENT_ID: string
-        SCRIPT_ID: string
-    }
-}
